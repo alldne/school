f4302121d3b8c170a84d80cfea62b845db0f57df
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-02-26 15:32:05 +0900

    Move BuiltinFunctions class into a separate file.

--- School/Evaluator.cs
+++ School/Evaluator.cs
@@ -10,49 +10,6 @@ namespace School
         }
     }
 
-    public static class BuiltinFunctions 
-    {
-        public static Value Add(Value a, Value b)
-        {
-            IntValue aValue = a as IntValue;
-            IntValue bValue = b as IntValue;
-            if (aValue == null || bValue == null)
-                throw new RuntimeTypeError("int expected");
-
-            return new IntValue(aValue.Value + bValue.Value);
-        }
-
-        public static Value Sub(Value a, Value b)
-        {
-            IntValue aValue = a as IntValue;
-            IntValue bValue = b as IntValue;
-            if (aValue == null || bValue == null)
-                throw new RuntimeTypeError("int expected");
-
-            return new IntValue(aValue.Value - bValue.Value);
-        }
-
-        public static Value Mul(Value a, Value b)
-        {
-            IntValue aValue = a as IntValue;
-            IntValue bValue = b as IntValue;
-            if (aValue == null || bValue == null)
-                throw new RuntimeTypeError("int expected");
-
-            return new IntValue(aValue.Value * bValue.Value);
-        }
-
-        public static Value Div(Value a, Value b)
-        {
-            IntValue aValue = a as IntValue;
-            IntValue bValue = b as IntValue;
-            if (aValue == null || bValue == null)
-                throw new RuntimeTypeError("int expected");
-
-            return new IntValue(aValue.Value / bValue.Value);
-        }
-    }
-
     public class Evaluator : Core.IExprVisitor<Value>
     {
         private static readonly Dictionary<string, Func<Value, Value, Value>> builtinFunctions = new Dictionary<string, Func<Value, Value, Value>>
