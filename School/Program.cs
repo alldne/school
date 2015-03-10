afa0fc0cce00584bac6cc6b6cd8d254d76673e59
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-03-10 10:29:10 +0900

    Add a simple REPL and start it from MainClass.

--- School/Program.cs
+++ School/Program.cs
@@ -6,23 +6,8 @@ namespace School
     {
         public static void Main(string[] args)
         {
-            var input = "(fun x y -> x + y end) 1 2";
-            Console.Write("Input:\n {0}\n\n", input);
-
-            var lexer = new SchoolLexer(input);
-            var parser = new SchoolParser(lexer);
-            Surface.Expr expr = parser.Parse();
-
-            Console.WriteLine("Print:");
-            var printer = new Printer();
-            printer.Print(expr);
-            Console.Write("\n\n");
-
-            var desugarer = new Desugarer();
-            Core.Expr coreExpr = desugarer.Desugar(expr);
-
-            var evaluator = new Evaluator();
-            Console.Write("Result:\n{0}\n\n", evaluator.Evaluate(coreExpr));
+            REPL repl = new REPL();
+            repl.Run();
         }
     }
 }
---
+++ School/REPL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace School
+{
+    public class REPL
+    {
+        public REPL()
+        {
+        }
+
+        public void Run()
+        {
+            Evaluator evaluator = new Evaluator();
+            string line;
+
+            Console.WriteLine("School REPL:");
+            do
+            {
+                Console.Write("> ");
+                line = Console.ReadLine();
+                if (!String.IsNullOrEmpty(line))
+                {
+                    try {
+                        Value value = evaluator.Evaluate(line);
+                        Console.WriteLine(value);
+                    } catch (Exception e) {
+                        Console.WriteLine(e);
+                    }
+                }
+            } while (line != null);
+        }
+    }
+}
+
