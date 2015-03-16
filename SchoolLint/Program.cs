a2b2c8e4f37ec7cd843d3a29d8448fd345346852
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-03-16 09:32:15 +0900

    Add SchoolLint project.

---
+++ SchoolLint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolLint
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
